80f2700cbcc53189c8a422b9d139ea021c3ae495
uvsq22004307 <71724431+uvsq22004307@users.noreply.github.com>
2021-02-16 10:03:23 +0100

    Create incendie.py

---
+++ incendie.py
@@ -0,0 +1,9 @@
+###################################################
+# Groupe BI: 1
+# Mathieu LAM
+# Océane MACHADO
+# Aurélie ALVET
+# Timothé PEYREIGNE
+# Thushanth JEYAKANTHN
+# https://github.com/uvsq22004307/projet_incendie
+###################################################
