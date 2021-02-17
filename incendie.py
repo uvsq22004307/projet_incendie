b3b5a7e7b9d37e9c4f00555114a29ce63139a0d6
Aurélie Alvet <aurelie.alvet@aurelilys.fr>
2021-02-18 00:58:37 +0100

    Reformatting code & comments

--- incendie.py
+++ incendie.py
@@ -9,64 +9,59 @@
 ###################################################
 
 
-
-#import des librairies
+# Import des librairies
 
 import tkinter as tk
-root = tk.Tk()
-root.title("Projet Incendie")
 from random import random
+
 import numpy as np
-canvas = tk.Canvas( width = 500, height = 500)
 
 
-#définition des constantes (écrites en majuscule)
+# Définition des constantes (écrites en majuscule)
 
 
+# Définition des variables globales
 
 
-#définition des variables globales
-
-
-
-
-#définition des fonctions (chaque fonction devra contenir une docstring)
+# Définition des fonctions (chaque fonction devra contenir une docstring)
 
 
 def hasard(p):
-   "renvoie True avec une probabilité p et False avec une probabilité 1-p"
-   r=random() # on prend au hasard un nombre entre 0 et 1
-   assert p>=0 and p<=1  # verifions que p est dans [0,1]
-   return r <= p # si r<=p (une proba p) on retourne True sinon on retourne False (une proba 1-p)
-             
+    """Renvoie True avec une probabilité p et False avec une probabilité 1-p"""
+    assert 0 <= p <= 1  # verifions que p est dans [0,1]
 
-def creerForet(x,y,pcocup):
-    "cree une foret avec des arbres et de l'eau placés aleatoirements"
-    foret=np.zeros((x,y)) # on cree un matrice x*y de zeros
+    r = random()  # On prend au hasard un nombre entre 0 et 1
+    return r <= p  # Si r<=p (une proba p) on retourne True sinon on retourne False (une proba 1-p)
+
+
+def creerForet(x, y, pcocup):
+    """Cree une foret avec des arbres et de l'eau placés aleatoirements"""
+    foret = np.zeros((x, y))  # on cree un matrice x*y de zeros
     for i in range(x):
         for j in range(y):
             if hasard(pcocup):
-                foret[i,j]=1. # si on une proba p alors il y a un arbre
+                foret[i, j] = 1.  # Si on une proba p alors il y a un arbre
             else:
-                foret[i,j]=0. # sinon il n'y a pas d'arbre mais de l'eau
-    return foret
-
-def mettre_le_feu(foret,i,j):
-    "met le feu a un arbre"
-    if foret[i,j]==1:
-        foret[i,j]=2. # on met le feu la case d'indice  (i,j)
+                foret[i, j] = 0.  # Sinon il n'y a pas d'arbre mais de l'eau
     return foret
 
 
+def mettre_le_feu(foret, i, j):
+    """Met le feu a un arbre"""
+    if foret[i, j] == 1:
+        foret[i, j] = 2.  # On met le feu la case d'indice  (i,j)
+    return foret
 
-#programme principal contenant la définition des widgets et des événements qui leur sont liés et l’appel à la boule de gestion des événements
 
-foret = tk.Button(root, text="crée une foret", command= creerForet() )
+# Programme principal contenant la définition des widgets et des événements qui leur sont liés et l’appel à la boule de gestion des événements
+root = tk.Tk()
+root.title("Projet Incendie")
+
+canvas = tk.Canvas(width=500, height=500)
+
+foret = tk.Button(root, text="crée une foret", command=creerForet())
 foret.pack()
-feu = tk.Button(root, text="crée une foret", command= mettre_le_feu() )
+feu = tk.Button(root, text="crée une foret", command=mettre_le_feu())
 feu.pack()
-
-
-
 
 root.mainloop()
